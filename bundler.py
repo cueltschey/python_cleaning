38fa18a962e6f40e4a5fafad8b72b97dbb61bee6
cueltschey <chaseuelt@gmail.com>
2023-12-03 14:28:50 -0600

    added bundler.py

--- bundler.py
+++ bundler.py
@@ -1,5 +1,29 @@
 import glob
 import shutil
 import os
+import sys
 
-print(os.getcwd())
+
+def bundle():
+    root = os.getcwd()
+
+    files = [f for f in glob.glob(root + "/*")]
+
+    args = sys.argv
+
+    if(len(args) < 2):
+        print("Usage: $bundler <bundle name>")
+        return False
+
+    os.mkdir(root + "/" + args[1]) 
+
+    for file in files:
+        if("." in file and file.split(".")[1] not in ["ini","dll"]):
+            new = root + f"/{args[1]}/" + file.split("/")[-1]
+            print(file)
+            print(new)
+            shutil.move(file, new)
+    return True
+
+if(bundle()):
+    print("bundling successful")
